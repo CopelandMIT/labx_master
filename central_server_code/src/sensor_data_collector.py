0e1f7777629dd3a6e1373dd25f3a5de4a4e1a960
CopelandMIT <dcope@mit.edu>
2025-01-14 15:05:31 -0500

    fixed date issue in phase sync

--- central_server_code/src/sensor_data_collector.py
+++ central_server_code/src/sensor_data_collector.py
@@ -34,7 +34,7 @@ rpi_hosts = [
 ]
 
 # 2) Local root folder to which you want to copy the folder from each RPi.
-DEST_ROOT = r'/media/daniel/FourTBLaCie/LabXData/fetal_pig_test'
+DEST_ROOT = r'/media/daniel/FourTBLaCie/LabXData/actuator_sync_test_no_gps'
 
 
 # ------------------------------------------------------------------------------
